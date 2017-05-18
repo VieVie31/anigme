5c21775ed763038a2af8a760eceede16d4b4e088
Sylvain <sylvain.maillot1@gmail.com>
2017-05-18 17:04:05 +0200

    Start to work on account creation and login

--- js/login.js
+++ js/login.js
@@ -28,3 +28,29 @@ function user_sign_in() {
 		toastr.error(errorMessage, "Sign In Error");
 	});
 }
+
+function user_create_account() {
+	var email = $("#mail_input_sing_up").val();
+	var password = $("#pass_input_sing_up1").val();
+	//check mdp
+	if (password != $("#pass_input_sing_up2").val()) {
+		toastr.error("Password is not the same");
+		return;
+	}
+
+	firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+	    current_user = firebase.auth().currentUser;
+
+	    //create a version number...
+		var database = firebase.database();
+		var t = database.ref('users/' + current_user.uid);
+		t.set({version: 0});
+	    
+	    toastr.info("Your account is now live... Have fun !!");
+
+	}, function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		toastr.error(errorMessage, "Account Creation Error");
+	});
+}
